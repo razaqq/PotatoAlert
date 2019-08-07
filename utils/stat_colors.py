98218f971abe32d074f9bc685c7b8e34d76a41c3
razaqq <paul.lais@web.de>
2019-08-07 23:23:14 +0200

    changed thresholds of colors to >=

--- utils/stat_colors.py
+++ utils/stat_colors.py
@@ -2,18 +2,18 @@ from assets.colors import Purple, Pink, Cyan, DGreen, LGreen, Yellow, Orange, Re
 
 
 def color_winrate(wr):
-    c = Purple() if wr > 65 else Pink() if wr > 60 else Cyan() if wr > 56 else DGreen() if wr > 54 else \
-        LGreen() if wr > 52 else Yellow() if wr > 49 else Orange() if wr > 47 else Red()
+    c = Purple() if wr >= 65 else Pink() if wr >= 60 else Cyan() if wr >= 56 else DGreen() if wr >= 54 else \
+        LGreen() if wr >= 52 else Yellow() if wr >= 49 else Orange() if wr >= 47 else Red()
     return c
 
 
 def color_battles(battles):
-    c = Purple() if battles > 20000 else Cyan() if battles > 14000 else LGreen() if battles > 9000 else \
-        Yellow() if battles > 5000 else Orange() if battles > 2000 else Red()
+    c = Purple() if battles >= 20000 else Cyan() if battles >= 14000 else LGreen() if battles >= 9000 else \
+        Yellow() if battles >= 5000 else Orange() if battles >= 2000 else Red()
     return c
 
 
 def color_avg_dmg(avg_dmg):
-    c = Pink() if avg_dmg > 48500 else Cyan() if avg_dmg > 38000 else LGreen() if avg_dmg > 33000 else \
-        Yellow() if avg_dmg > 22000 else Orange() if avg_dmg > 16000 else Red()
+    c = Pink() if avg_dmg >= 48500 else Cyan() if avg_dmg >= 38000 else LGreen() if avg_dmg >= 33000 else \
+        Yellow() if avg_dmg >= 22000 else Orange() if avg_dmg >= 16000 else Red()
     return c
