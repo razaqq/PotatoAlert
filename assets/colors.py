b798dbbfe836e94b88f7262f1eed72160145640e
razaqq <paul.lais@web.de>
2019-08-12 20:37:39 +0200

    added alpha value

--- assets/colors.py
+++ assets/colors.py
@@ -24,50 +24,50 @@ from PyQt5.QtGui import QColor
 
 
 class Orange(QColor):
-    def __init__(self):
-        super().__init__(255, 149, 0)
+    def __init__(self, alpha):
+        super().__init__(255, 149, 0, alpha)
 
 
 class Purple(QColor):
-    def __init__(self):
-        super().__init__(124, 8, 130)
+    def __init__(self, alpha):
+        super().__init__(124, 8, 130, alpha)
 
 
 class Pink(QColor):
-    def __init__(self):
-        super().__init__(222, 37, 232)
+    def __init__(self, alpha):
+        super().__init__(222, 37, 232, alpha)
 
 
 class Cyan(QColor):
-    def __init__(self):
-        super().__init__(63, 224, 214)
+    def __init__(self, alpha):
+        super().__init__(63, 224, 214, alpha)
 
 
 class Red(QColor):
-    def __init__(self):
-        super().__init__(199, 10, 10)
+    def __init__(self, alpha):
+        super().__init__(199, 10, 10, alpha)
 
 
 class LGreen(QColor):
-    def __init__(self):
-        super().__init__(23, 209, 51)
+    def __init__(self, alpha):
+        super().__init__(23, 209, 51, alpha)
 
 
 class DGreen(QColor):
-    def __init__(self):
-        super().__init__(1, 126, 20)
+    def __init__(self, alpha):
+        super().__init__(1, 126, 20, alpha)
 
 
 class Grey(QColor):
-    def __init__(self):
-        super().__init__(224, 222, 218)
+    def __init__(self, alpha):
+        super().__init__(224, 222, 218, alpha)
 
 
 class Yellow(QColor):
-    def __init__(self):
-        super().__init__(255, 208, 18)
+    def __init__(self, alpha):
+        super().__init__(255, 208, 18, alpha)
 
 
 class White(QColor):
-    def __init__(self):
-        super().__init__(255, 255, 255)
+    def __init__(self, alpha):
+        super().__init__(255, 255, 255, alpha)
