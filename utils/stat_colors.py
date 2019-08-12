a8cb42d216d1deaa73aa43077608ff0345d393f4
razaqq <paul.lais@web.de>
2019-08-12 20:47:32 +0200

    added personal rating

--- utils/stat_colors.py
+++ utils/stat_colors.py
@@ -17,3 +17,10 @@ def color_avg_dmg(avg_dmg):
     c = Pink() if avg_dmg >= 48500 else Cyan() if avg_dmg >= 38000 else LGreen() if avg_dmg >= 33000 else \
         Yellow() if avg_dmg >= 22000 else Orange() if avg_dmg >= 16000 else Red()
     return c
+
+
+def color_personal_rating(pr):
+    c = Purple(100) if pr > 2450 else Pink(100) if pr > 2100 else Cyan(100) if pr > 1750 else \
+        DGreen(100) if pr > 1550 else LGreen(100) if pr > 1350 else Yellow(100) if pr > 1100 else \
+        Orange(100) if pr > 750 else Red(100)
+    return c
