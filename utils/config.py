fb26dfb253af52f4ad1e5e9e889ada4ddcf9a2d6
razaqq <paul.lais@web.de>
2019-10-23 17:50:25 +0200

    added entry for additional infos

--- utils/config.py
+++ utils/config.py
@@ -75,6 +75,9 @@ class Config(ConfigParser):
         if 'WindowH' not in self['DEFAULT']:
             ok = False
             self['DEFAULT']['WindowH'] = '450'
+        if 'additional_infos' not in self['DEFAULT']:
+            ok = False
+            self['DEFAULT']['additional_infos'] = 'false'
         self.save()
         if not ok:
             self.read_config()
