3a282ba078a0fdd9acdc91371351cc90bd76ba63
razaqq <paul.lais@web.de>
2019-10-23 11:10:11 +0200

    updated colors

--- assets/colors.py
+++ assets/colors.py
@@ -23,39 +23,56 @@ SOFTWARE.
 from PyQt5.QtGui import QColor
 
 
+def hex_to_rgb(hex_code):
+    hex_code = hex_code.lstrip('#')
+    hlen = len(hex_code)
+    return tuple(int(hex_code[i:i + hlen // 3], 16) for i in range(0, hlen, hlen // 3))
+
+
+# Ship specific stats could be pulled from https://api.wows-numbers.com/colors/json/
+red = hex_to_rgb('#FE0E00')
+orange = hex_to_rgb('#FE7903')
+yellow = hex_to_rgb('#FFC71F')
+lgreen = hex_to_rgb('#17D133')  # default #44B300
+dgreen = hex_to_rgb('#017E14')  # default #318000
+cyan = hex_to_rgb('#02C9B3')
+pink = hex_to_rgb('#D042F3')
+purple = hex_to_rgb('#A00DC5')
+
+
 class Orange(QColor):
     def __init__(self, alpha=255):
-        super().__init__(255, 149, 0, alpha)
+        super().__init__(*orange, alpha)
 
 
 class Purple(QColor):
     def __init__(self, alpha=255):
-        super().__init__(124, 8, 130, alpha)
+        super().__init__(*purple, alpha)
 
 
 class Pink(QColor):
     def __init__(self, alpha=255):
-        super().__init__(222, 37, 232, alpha)
+        super().__init__(*pink, alpha)
 
 
 class Cyan(QColor):
     def __init__(self, alpha=255):
-        super().__init__(63, 224, 214, alpha)
+        super().__init__(*cyan, alpha)
 
 
 class Red(QColor):
     def __init__(self, alpha=255):
-        super().__init__(199, 10, 10, alpha)
+        super().__init__(*red, alpha)
 
 
 class LGreen(QColor):
     def __init__(self, alpha=255):
-        super().__init__(23, 209, 51, alpha)
+        super().__init__(*lgreen, alpha)
 
 
 class DGreen(QColor):
     def __init__(self, alpha=255):
-        super().__init__(1, 126, 20, alpha)
+        super().__init__(*dgreen, alpha)
 
 
 class Grey(QColor):
@@ -65,7 +82,7 @@ class Grey(QColor):
 
 class Yellow(QColor):
     def __init__(self, alpha=255):
-        super().__init__(255, 208, 18, alpha)
+        super().__init__(*yellow, alpha)
 
 
 class White(QColor):
