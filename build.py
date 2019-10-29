eed05af0ae137371529c9bbb7f0714ba0b79a02a
razaqq <paul.lais@web.de>
2019-10-29 19:06:55 +0100

    updated build.py

--- build.py
+++ build.py
@@ -24,9 +24,6 @@ import os
 import sys
 import subprocess
 
-ADDITIONAL_LIB_PATHS = 'C:\\Program Files (x86)\\Windows Kits\\10\\Redist\\ucrt\\DLLs\\x64'
-QT5_LIBS_PATH = 'C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python37\\Lib\\site - packages\\PyQt5\\Qt\\bin'
-
 
 if __name__ == '__main__':
     root = os.path.abspath(os.path.dirname(__file__))
@@ -39,7 +36,7 @@ if __name__ == '__main__':
     build_flags = '-F -y -w'
 
     build = f'{sys.executable} -m PyInstaller {build_flags} -i "{icon}" --add-data "{assets}"{assets_sep}"assets/" ' \
-            f'--paths "{ADDITIONAL_LIB_PATHS}" --paths "{QT5_LIBS_PATH}" "{main}"'
+            f'"{main}"'
 
     subprocess.call(
         build
