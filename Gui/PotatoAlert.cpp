1ff15e6f1eb7b6a58ad864ae5350659d2d8e6ef4
razaq <paul.lais@web.de>
2021-10-30 12:32:51 +0200

    set high dpi scaling before app creation

--- Gui/PotatoAlert.cpp
+++ Gui/PotatoAlert.cpp
@@ -29,13 +29,14 @@ static int RunMain(int argc, char* argv[])
 
 	PotatoAlert::Log::Init();
 
+	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
 	QApplication app(argc, argv);
 	
 	QApplication::setOrganizationName(PRODUCT_COMPANY_NAME);
 	QApplication::setApplicationVersion(PRODUCT_VERSION_FULL_STR);
 
-	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-	QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 
 	QFile file(":/style.qss");
 	file.open(QFile::ReadOnly | QFile::Text);
