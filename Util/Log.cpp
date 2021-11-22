e93d803093d9e823ae5904b2c6b6d587f59f68b1
razaq <paul.lais@web.de>
2021-11-22 18:30:21 +0100

    log to stdout in release mode too

--- Util/Log.cpp
+++ Util/Log.cpp
@@ -57,22 +57,21 @@ void Log::Init()
 	const std::string filePath = QDir(GetDir()).filePath("PotatoAlert.log").toStdString();
 	spdlog::set_error_handler([](const std::string& msg) { spdlog::get("console")->error("*** LOGGER ERROR ***: {}", msg); });
 
-#ifndef NDEBUG
 	auto stdoutSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 	stdoutSink->set_pattern("%^[%T] %n: %v%$");
+
+#ifndef NDEBUG
 	stdoutSink->set_level(spdlog::level::trace);
+#else
+	stdoutSink->set_level(spdlog::level::info);
 #endif
 
 	auto fileSink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(filePath);
 	fileSink->set_pattern("[%d-%m-%Y %T] [%=7l] %n (%-30!@): %v");
 	fileSink->set_level(spdlog::level::info);
 
-#ifndef NDEBUG
 	std::vector<spdlog::sink_ptr> sinks{ stdoutSink, fileSink };
 	s_logger = std::make_shared<spdlog::logger>("PotatoAlert", sinks.begin(), sinks.end());
-#else
-	s_logger = std::make_shared<spdlog::logger>("PotatoAlert", fileSink);
-#endif
 
 	spdlog::register_logger(s_logger);
 	s_logger->set_level(spdlog::level::trace);
