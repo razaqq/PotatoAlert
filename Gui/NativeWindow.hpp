bf9d31b61ab136e8061913dfd2d445b17b9bbbf0
razaq <paul.lais@web.de>
2022-02-01 12:17:39 +0100

    add native window focus event

--- Gui/NativeWindow.hpp
+++ Gui/NativeWindow.hpp
@@ -13,17 +13,21 @@ namespace PotatoAlert::Gui {
 class NativeWindow : public QWidget
 {
 	Q_OBJECT
+
 public:
 	explicit NativeWindow(QMainWindow* mainWindow);
+	static void RequestFocus();
+
 private:
 	void Init();
 	QMainWindow* m_mainWindow;
 
-	static const int m_borderWidth = 4;
+	static constexpr int m_borderWidth = 4;
 
 	TitleBar* m_titleBar = new TitleBar(this);
 
 	void closeEvent(QCloseEvent* event) override;
+	bool nativeEvent(const QByteArray& eventType, void* message, long* result) override;
 };
 
 }  // namespace PotatoAlert::Gui
