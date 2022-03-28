c87f5cc54b1fa35e9e7f32337a6f3de37ea39f7e
razaq <paul.lais@web.de>
2022-03-28 21:40:28 +0200

    add filemapping abstractions

---
+++ Core/include/Core/FileMapping.hpp
@@ -0,0 +1,113 @@
+// Copyright 2022 <github.com/razaqq>
+#pragma once
+
+#include "Core/File.hpp"
+#include "Core/Flags.hpp"
+
+#include <string>
+#include <utility>
+
+
+namespace PotatoAlert::Core {
+
+class FileMapping
+{
+public:
+	enum class Handle : uintptr_t
+	{
+		Null = 0
+	};
+
+	enum class Flags : uint32_t
+	{
+		Read    = 0x1,
+		Write   = 0x2,
+		Execute = 0x4,
+		None    = 0x8
+	};
+
+	FileMapping() : m_handle(Handle::Null) {}
+
+	explicit FileMapping(Handle handle) : m_handle(handle) {}
+
+	FileMapping(FileMapping&& src) noexcept
+	{
+		m_handle = std::exchange(src.m_handle, Handle::Null);
+	}
+
+	FileMapping(const FileMapping&) = delete;
+
+	FileMapping& operator=(FileMapping&& src) noexcept
+	{
+		if (m_handle != Handle::Null)
+			RawClose(m_handle);
+		m_handle = std::exchange(src.m_handle, Handle::Null);
+		return *this;
+	}
+
+	FileMapping& operator=(const FileMapping&) = delete;
+
+	~FileMapping()
+	{
+		if (m_handle != Handle::Null)
+			RawClose(m_handle);
+	}
+
+	[[nodiscard]] Handle GetHandle() const
+	{
+		return m_handle;
+	}
+
+	static FileMapping Open(const File& file, Flags flags, uint64_t maxSize)
+	{
+		return FileMapping(RawOpen(file.GetHandle(), flags, maxSize));
+	}
+
+	void Close()
+	{
+		RawClose(std::exchange(m_handle, Handle::Null));
+	}
+
+	void* Map(Flags flags, uint64_t offset, size_t size)
+	{
+		return RawMap(m_handle, flags, offset, size);
+	}
+
+	void Unmap(const void* view, size_t size)
+	{
+		return RawUnmap(m_handle, view, size);
+	}
+
+	[[nodiscard]] static std::string LastError();
+
+	[[nodiscard]] bool IsOpen() const
+	{
+		return m_handle != Handle::Null;
+	}
+
+	explicit operator bool() const
+	{
+		return m_handle != Handle::Null;
+	}
+
+	bool operator==(decltype(nullptr)) const
+	{
+		return m_handle == Handle::Null;
+	}
+
+	bool operator!=(decltype(nullptr)) const
+	{
+		return m_handle != Handle::Null;
+	}
+
+private:
+	Handle m_handle;
+
+	static Handle RawOpen(File::Handle file, Flags flags, uint64_t maxSize);
+	static void RawClose(Handle handle);
+	static void* RawMap(Handle handle, Flags flags, uint64_t offset, size_t size);
+	static void RawUnmap(Handle handle, const void* view, size_t size);
+};
+DEFINE_FLAGS(FileMapping::Flags);
+
+}  // namespace PotatoAlert::Core
---
+++ Core/src/FileMapping.linux.cpp
@@ -0,0 +1,75 @@
+// Copyright 2022 <github.com/razaqq>
+
+#include "Core/File.hpp"
+#include "Core/FileMapping.hpp"
+
+#include <cassert>
+#include <sys/mman.h>
+#include <unistd.h>
+
+
+using PotatoAlert::Core::File;
+using PotatoAlert::Core::FileMapping;
+
+namespace {
+
+template<typename T>
+static constexpr T CreateHandle(int handle)
+{
+	return static_cast<T>(reinterpret_cast<uintptr_t>(handle) + 1);
+}
+
+template<typename T>
+static constexpr T UnwrapHandle(FileMapping::Handle handle)
+{
+	return reinterpret_cast<T>(static_cast<uintptr_t>(handle) - 1);
+}
+
+template<typename T>
+static constexpr T UnwrapHandle(File::Handle handle)
+{
+	return reinterpret_cast<T>(static_cast<uintptr_t>(handle) - 1);
+}
+
+}  // namespace
+
+std::string FileMapping::LastError()
+{
+	return "";
+}
+
+FileMapping::Handle FileMapping::RawOpen(File::Handle file, Flags flags, uint64_t maxSize)
+{
+}
+
+void FileMapping::RawClose(Handle handle)
+{
+}
+
+void* FileMapping::RawMap(Handle handle, Flags flags, uint64_t offset, size_t size)
+{
+	int prot = 0;
+
+	if (HasFlag(flags, Flags::Read))
+		prot |= PROT_READ;
+	if (HasFlag(flags, Flags::Write))
+		prot |= PROT_WRITE;
+	if (HasFlag(flags, Flags::Execute))
+		prot |= PROT_EXEC;
+	if (HasFlag(flags, Flags::None))
+		prot |= PROT_NONE;
+
+	int shareFlag = 0;
+
+	size_t length = 0;
+	long pageSize = sysconf(_SC_PAGE_SIZE);
+	void* addr = mmap(nullptr, length, prot, shareFlag, UnwrapHandle<int>(handle), offset);
+}
+
+void FileMapping::RawUnmap(Handle handle, const void* view, size_t size)
+{
+	if (munmap(const_cast<void*>(view), size) != 0)
+	{
+		// TODO: handle error
+	}
+}
