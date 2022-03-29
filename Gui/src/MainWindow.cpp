57958e5bd79690861bd9ff11734dd62387d3c2ca
razaq <paul.lais@web.de>
2022-03-29 14:58:39 +0200

    use QuestionDialog in main window

--- Gui/src/MainWindow.cpp
+++ Gui/src/MainWindow.cpp
@@ -8,18 +8,14 @@
 #include "Core/Log.hpp"
 #include "Core/Screenshot.hpp"
 
-#include "Gui/FramelessDialog.hpp"
 #include "Gui/MainWindow.hpp"
 #include "Gui/MatchHistory.hpp"
 #include "Gui/MenuBar/VerticalMenuBar.hpp"
+#include "Gui/QuestionDialog.hpp"
 #include "Gui/StatsWidget/StatsWidget.hpp"
 
-#include <QApplication>
 #include <QDesktopServices>
-#include <QDialogButtonBox>
-#include <QIcon>
 #include <QMainWindow>
-#include <QPushButton>
 #include <QUrl>
 #include <QVBoxLayout>
 #include <QWidget>
@@ -30,6 +26,8 @@ using namespace PotatoAlert::Core;
 using PotatoAlert::Client::PotatoClient;
 using PotatoAlert::Gui::MainWindow;
 using PotatoAlert::Gui::MatchHistory;
+using PotatoAlert::Gui::QuestionAnswer;
+using PotatoAlert::Gui::QuestionDialog;
 
 MainWindow::MainWindow() : QMainWindow()
 {
@@ -136,49 +134,11 @@ void MainWindow::ConnectSignals()
 
 bool MainWindow::ConfirmUpdate()
 {
-	auto dialog = new FramelessDialog(this);
-
 	/*
 	 * TODO: FIX WARNING
 	 * QWindowsWindow::setGeometry: Unable to set geometry
 	*/
 
-	auto buttonBox = new QDialogButtonBox();
-	buttonBox->setAttribute(Qt::WA_TranslucentBackground);
-
-	auto yesButton = new QPushButton(GetString(StringTable::Keys::YES), buttonBox);
-	yesButton->setObjectName("confirmButton");
-
-	auto noButton = new QPushButton(GetString(StringTable::Keys::NO), buttonBox);
-	noButton->setObjectName("confirmButton");
-
-	connect(yesButton, &QPushButton::clicked, [dialog]([[maybe_unused]] bool checked) { dialog->done(QDialog::Accepted); });
-	connect(noButton, &QPushButton::clicked, [dialog]([[maybe_unused]] bool checked) { dialog->done(QDialog::Rejected); });
-
-	buttonBox->addButton(yesButton, QDialogButtonBox::ActionRole);
-	buttonBox->addButton(noButton, QDialogButtonBox::ActionRole);
-	buttonBox->setCenterButtons(true);
-
-	auto textField = new QLabel(GetString(StringTable::Keys::UPDATE_QUESTION));
-	textField->setWordWrap(true);
-
-	auto icon = new QIcon(QApplication::style()->standardIcon(QStyle::SP_MessageBoxQuestion));
-	auto iconLabel = new QLabel();
-	iconLabel->setPixmap(icon->pixmap(100, 100));
-
-	auto layout = new QVBoxLayout();
-	layout->setContentsMargins(15, 15, 15, 10);
-
-	auto textLayout = new QHBoxLayout();
-	textLayout->addWidget(iconLabel, 0, Qt::AlignRight);
-	textLayout->addWidget(textField, 0, Qt::AlignLeft);
-
-	layout->addLayout(textLayout);
-	layout->addWidget(buttonBox,0, Qt::AlignHCenter);
-
-	dialog->setLayout(layout);
-
-	dialog->show();
-
-	return dialog->exec();
+	auto dialog = new QuestionDialog(this, GetString(StringTable::Keys::UPDATE_QUESTION));
+	return dialog->Run() == QuestionAnswer::Yes;
 }
