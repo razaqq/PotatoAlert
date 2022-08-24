765960a6b9b3f046e20ae2bec57c1c7b75f4d7c7
razaq <paul.lais@web.de>
2022-08-24 20:45:00 +0200

    add json serialize enum macros

--- Core/include/Core/Json.hpp
+++ Core/include/Core/Json.hpp
@@ -18,6 +18,55 @@
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
+#define NLOHMANN_JSON_SERIALIZE_ENUM_PAIRS(ENUM_TYPE, PAIRS)                                 \
+	template<typename BasicJsonType>                                                         \
+	inline void to_json(BasicJsonType& j, const ENUM_TYPE& e)                                \
+	{                                                                                        \
+		static_assert(std::is_enum<ENUM_TYPE>::value, #ENUM_TYPE " must be an enum!");       \
+		auto it = std::find_if(std::begin(PAIRS), std::end(PAIRS),                           \
+							[e](const std::pair<ENUM_TYPE, BasicJsonType>& ej_pair) -> bool  \
+							{                                                                \
+								return ej_pair.first == e;                                   \
+							});                                                              \
+		j = ((it != std::end(PAIRS)) ? it : std::begin(PAIRS))->second;                      \
+	}                                                                                        \
+	template<typename BasicJsonType>                                                         \
+	inline void from_json(const BasicJsonType& j, ENUM_TYPE& e)                              \
+	{                                                                                        \
+		static_assert(std::is_enum<ENUM_TYPE>::value, #ENUM_TYPE " must be an enum!");       \
+		auto it = std::find_if(std::begin(PAIRS), std::end(PAIRS),                           \
+							[&j](const std::pair<ENUM_TYPE, BasicJsonType>& ej_pair) -> bool \
+							{                                                                \
+								return ej_pair.second == j;                                  \
+							});                                                              \
+		e = ((it != std::end(PAIRS)) ? it : std::begin(PAIRS))->first;                       \
+	}
+
+#define NLOHMANN_JSON_SERIALIZE_ENUM_MAP(ENUM_TYPE, MAP)                                     \
+	template<typename BasicJsonType>                                                         \
+	inline void to_json(BasicJsonType& j, const ENUM_TYPE& e)                                \
+	{                                                                                        \
+		static_assert(std::is_enum<ENUM_TYPE>::value, #ENUM_TYPE " must be an enum!");       \
+		if ((MAP).contains(e))                                                               \
+		{                                                                                    \
+			j = (MAP).at(e);                                                                 \
+		}                                                                                    \
+		else                                                                                 \
+		{                                                                                    \
+			j = (MAP).begin()->second;                                                       \
+		}                                                                                    \
+	}                                                                                        \
+	template<typename BasicJsonType>                                                         \
+	inline void from_json(const BasicJsonType& j, ENUM_TYPE& e)                              \
+	{                                                                                        \
+		static_assert(std::is_enum<ENUM_TYPE>::value, #ENUM_TYPE " must be an enum!");       \
+		auto it = std::find_if(std::begin(MAP), std::end(MAP),                               \
+							[&j](const std::pair<ENUM_TYPE, BasicJsonType>& ej_pair) -> bool \
+							{                                                                \
+								return ej_pair.second == j;                                  \
+							});                                                              \
+		e = ((it != std::end(MAP)) ? it : std::begin(MAP))->first;                           \
+	}
 
 class sax_no_exception : public nlohmann::detail::json_sax_dom_parser<json>
 {
