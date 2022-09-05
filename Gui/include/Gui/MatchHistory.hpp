291978fff09c3be29c331819e46cd590a5314cc4
razaq <paul.lais@web.de>
2022-09-06 01:18:03 +0200

    update match history gui

--- Gui/include/Gui/MatchHistory.hpp
+++ Gui/include/Gui/MatchHistory.hpp
@@ -4,6 +4,8 @@
 #include "Client/MatchHistory.hpp"
 #include "Client/StatsParser.hpp"
 
+#include "Gui/IconButton.hpp"
+
 #include "ReplayParser/ReplayParser.hpp"
 
 #include <QEvent>
@@ -23,21 +25,30 @@ public:
 	explicit MatchHistory(QWidget* parent = nullptr);
 	void UpdateAll();
 	void UpdateLatest();
-	void SetSummary(uint32_t id, const ReplaySummary& summary) const;
+	void SetSummary(uint32_t id, const ReplayParser::ReplaySummary& summary) const;
 
 private:
+	struct GuiEntry
+	{
+		std::array<QTableWidgetItem*, 7> Fields;
+		IconButton* Button;
+	};
+
 	void Init();
 	void InitHeaders() const;
 	void changeEvent(QEvent* event) override;
 	void paintEvent(QPaintEvent* _) override;
 	QTableWidget* m_table = new QTableWidget();
 	void AddEntry(const Client::MatchHistory::Entry& entry);
-	std::map<uint32_t, std::array<QTableWidgetItem*, 7>> m_entries;
+	std::map<uint32_t, GuiEntry> m_entries;
 	QPushButton* m_deleteButton = new QPushButton();
+	int m_btnColumn;
+	int m_jsonColumn;
 	// QPushButton* m_analyzeButton = new QPushButton();
 
 signals:
-	void ReplaySelected(const Client::StatsParser::Match& match);
+	void ReplaySelected(const Client::StatsParser::MatchType& match);
+	void ReplaySummarySelected(const Client::MatchHistory::Entry& entry);
 };
 
 }  // namespace PotatoAlert::Gui
