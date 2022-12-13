015ebe35a41d0004f3012808bade675adea4aa69
razaq <paul.lais@web.de>
2022-12-13 15:07:37 +0100

    blur replay summary background

--- Gui/include/Gui/ReplaySummary.hpp
+++ Gui/include/Gui/ReplaySummary.hpp
@@ -6,61 +6,15 @@
 
 #include "Gui/AspectRatioWidget.hpp"
 
-#include <QGraphicsDropShadowEffect>
 #include <QLabel>
 #include <QPaintEvent>
-#include <QPainter>
 #include <QWidget>
 
 
 namespace PotatoAlert::Gui {
 
-class ShadowLabel : public QLabel
-{
-public:
-	explicit ShadowLabel(std::string_view text, int shadowX = 0, int shadowY = 0, int blurRadius = 0,
-						 QColor shadowColor = QColor(0, 0, 0), QWidget* parent = nullptr) : QLabel(text.data(), parent)
-	{
-		QGraphicsDropShadowEffect* effect = new QGraphicsDropShadowEffect(this);
-		effect->setBlurRadius(blurRadius);
-		effect->setColor(shadowColor);
-		effect->setOffset(shadowX, shadowY);
-		setGraphicsEffect(effect);
-		setObjectName("ReplaySummary_ShadowLabel");
-	}
-};
-
-class Background : public QWidget
-{
-public:
-	explicit Background(const char* map, QWidget* parent = nullptr)
-		: QWidget(parent), m_map(map) {}  // m_nationFlag(nationFlag) {}
-
-	void SetImage(const QString& map)  // , const QString& nationFlag)
-	{
-		m_map = map;
-		// m_nationFlag = nationFlag;
-		update();
-		repaint();
-	}
-
-protected:
-	void paintEvent(QPaintEvent* event) override
-	{
-		QPainter painter(this);
-		QPixmap map = QPixmap::fromImage(QImage(m_map));
-		// QPixmap flag = QPixmap::fromImage(QImage(m_nationFlag));
-		painter.drawPixmap(0, 0, map.scaledToHeight(height(), Qt::SmoothTransformation));
-		// painter.drawPixmap(0, 0, flag.scaledToHeight(height(), Qt::SmoothTransformation));
-		QWidget::paintEvent(event);
-	}
-
-private:
-	QString m_map;
-	// QString m_nationFlag;
-	static constexpr float m_aspectRatio = 16.0f / 9.0f;
-};
-
+class ShadowLabel;
+class Background;
 
 class ReplaySummary : public QWidget
 {
--- Gui/src/ReplaySummary.cpp
+++ Gui/src/ReplaySummary.cpp
@@ -10,9 +10,11 @@
 
 #include <QApplication>
 #include <QFontDatabase>
+#include <QGraphicsDropShadowEffect>
 #include <QHBoxLayout>
 #include <QPainter>
 #include <QStyleOption>
+#include <QtWidgets/private/qpixmapfilter_p.h>
 
 
 using namespace PotatoAlert::Client::StringTable;
@@ -21,6 +23,7 @@ using PotatoAlert::ReplayParser::RibbonType;
 using PotatoAlert::Client::Config;
 using PotatoAlert::Client::ConfigKey;
 using PotatoAlert::Client::Match;
+using PotatoAlert::Gui::Background;
 using PotatoAlert::Gui::ShadowLabel;
 using ReplaySummaryData = PotatoAlert::ReplayParser::ReplaySummary;
 using ReplaySummaryGui = PotatoAlert::Gui::ReplaySummary;
@@ -137,6 +140,58 @@ static constexpr std::string_view GetRibbonName(int lang, RibbonType ribbon)
 }
 
 }  // namespace
+
+class Background : public QWidget
+{
+public:
+	explicit Background(const char* map, QWidget* parent = nullptr)
+		: QWidget(parent), m_map(map) {}  // m_nationFlag(nationFlag) {}
+
+	void SetImage(const QString& map)  // , const QString& nationFlag)
+	{
+		m_map = map;
+		// m_nationFlag = nationFlag;
+		update();
+		repaint();
+	}
+
+protected:
+	void paintEvent(QPaintEvent* event) override
+	{
+		QPainter painter(this);
+		QPixmap map = QPixmap::fromImage(QImage(m_map));
+
+		QPixmapBlurFilter f;
+		f.setRadius(4);
+		f.setBlurHints(QGraphicsBlurEffect::QualityHint);
+		f.draw(&painter, { 0, 0 }, map);
+
+		// QPixmap flag = QPixmap::fromImage(QImage(m_nationFlag));
+		// painter.drawPixmap(0, 0, map.scaledToHeight(height(), Qt::SmoothTransformation));
+		// painter.drawPixmap(0, 0, flag.scaledToHeight(height(), Qt::SmoothTransformation));
+		QWidget::paintEvent(event);
+	}
+
+private:
+	QString m_map;
+	// QString m_nationFlag;
+	static constexpr float m_aspectRatio = 16.0f / 9.0f;
+};
+
+class ShadowLabel : public QLabel
+{
+public:
+	explicit ShadowLabel(std::string_view text, int shadowX = 0, int shadowY = 0, int blurRadius = 0,
+						 QColor shadowColor = QColor(0, 0, 0), QWidget* parent = nullptr) : QLabel(text.data(), parent)
+	{
+		QGraphicsDropShadowEffect* effect = new QGraphicsDropShadowEffect(this);
+		effect->setBlurRadius(blurRadius);
+		effect->setColor(shadowColor);
+		effect->setOffset(shadowX, shadowY);
+		setGraphicsEffect(effect);
+		setObjectName("ReplaySummary_ShadowLabel");
+	}
+};
 
 class Ribbon : public QWidget
 {
