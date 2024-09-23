cbda1a92324aa7a395b5fd676a280ce1c4522ed3
razaq <paul.lais@web.de>
2024-09-23 15:38:44 +0200

    add type traits

--- Core/include/Core/TypeTraits.hpp
+++ Core/include/Core/TypeTraits.hpp
@@ -1,6 +1,9 @@
 // Copyright 2023 <github.com/razaqq>
 #pragma once
 
+#include <type_traits>
+
+
 namespace PotatoAlert::Core {
 
 template<typename...>
@@ -11,5 +14,11 @@ inline constexpr bool always_true = true;
 
 template<bool Value, typename...>
 inline constexpr bool bool_value = Value;
+
+template<typename T, typename... Ts>
+inline constexpr bool is_any_of_v = (std::is_same_v<T, Ts> || ...);
+
+template<typename T, typename... Ts>
+using is_any_of = std::bool_constant<is_any_of_v<T, Ts...>>;
 
 }
