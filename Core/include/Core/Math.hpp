af6ea65d283885e915b7a148bf1fe0fa4aeb4295
razaq <paul.lais@web.de>
2025-01-27 23:07:45 +0100

    add math functions

--- Core/include/Core/Math.hpp
+++ Core/include/Core/Math.hpp
@@ -1,17 +1,89 @@
 // Copyright 2021 <github.com/razaqq>
 #pragma once
 
+#include <cmath>
+
 
 namespace PotatoAlert::Core {
 
 struct Vec2
 {
 	float X, Y;
+
+	inline static constexpr Vec2 Null()
+	{
+		return { 0.0f, 0.0f };
+	}
+
+	inline constexpr bool IsNull() const
+	{
+		return *this == Null();
+	}
+
+	inline constexpr Vec2 operator-() const
+	{
+		return { -X, -Y };
+	}
+
+	inline bool operator==(const Vec2 other) const
+	{
+		constexpr float delta = 0.00001;
+		return std::abs(X - other.X) < delta && std::abs(Y - other.Y) < delta;
+	}
+
+	inline constexpr Vec2 operator+(const Vec2 other) const
+	{
+		return { X + other.X, Y + other.Y };
+	}
+
+	inline constexpr void operator+=(const Vec2 other)
+	{
+		X += other.X;
+		Y += other.Y;
+	}
+
+	inline float Distance(Vec2 other) const
+	{
+		return std::sqrt((X - other.X) * (X - other.X) + (Y - other.Y) * (Y - other.Y));
+	}
 };
 
 struct Vec3
 {
 	float X, Y, Z;
+
+	inline static constexpr Vec3 Null()
+	{
+		return { 0.0f, 0.0f, 0.0f };
+	}
+
+	inline constexpr bool IsNull() const
+	{
+		return *this == Null();
+	}
+
+	inline constexpr Vec3 operator-() const
+	{
+		return { -X, -Y, -Z };
+	}
+
+	inline bool operator==(const Vec3 other) const
+	{
+		constexpr float delta = 0.00001;
+		return std::abs(X - other.X) < delta && std::abs(Y - other.Y) < delta && std::abs(Z - other.Z) < delta;
+	}
+
+	inline constexpr Vec3 operator+(const Vec3 other) const
+	{
+		return { X + other.X, Y + other.Y, Z + other.Z };
+	}
+
+	inline constexpr void operator+=(const Vec3 other)
+	{
+		X += other.X;
+		Y += other.Y;
+		Z += other.Z;
+	}
 };
 
 struct Vec4
@@ -22,6 +94,11 @@ struct Vec4
 struct Rot3
 {
 	float Yaw, Pitch, Roll;
+
+	static constexpr Rot3 Null()
+	{
+		return { 0.0f, 0.0f, 0.0f };
+	}
 };
 
 struct Mat3
